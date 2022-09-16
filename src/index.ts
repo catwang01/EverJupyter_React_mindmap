23beefb70d68a06cbd8fba96d88553c53a9999b1
catwang01 <64242574+catwang01@users.noreply.github.com>
2022-09-16 12:47:39 +0800

    add tracker (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,42 @@
 // src/index.ts
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
+  ILayoutRestorer
 } from '@jupyterlab/application';
 
-import { ICommandPalette, MainAreaWidget, IFrame } from '@jupyterlab/apputils';
+import { ICommandPalette,  MainAreaWidget, IFrame, WidgetTracker } from '@jupyterlab/apputils';
 
 /**
  * Initialization data for the jupyterlab_apod extension.
 */
 
-const activateFunc = async (app: JupyterFrontEnd, palette: ICommandPalette) => {
+const activateFunc = async (app: JupyterFrontEnd, palette: ICommandPalette, restorer: ILayoutRestorer) => {
     console.log('JupyterLab extension EverJupyter_React_Mindmap is activated!');
 
-    // Create a blank content widget inside of a MainAreaWidget
-    const content = new IFrame({
-      sandbox: ['allow-downloads' , 'allow-forms' , 'allow-modals' , 'allow-orientation-lock' , 'allow-pointer-lock' , 'allow-popups' , 'popups-to-escape-sandbox' , 'allow-presentation' , 'allow-same-origin' , 'allow-scripts' , 'allow-storage-access-by-user-activation' , 'allow-top-navigation' , 'allow-top-navigation-by-user-activation'],
-      referrerPolicy: 'unsafe-url'
-    });
-    content.url = "http://localhost:8080/react-mindmap/index.html";
-    const widget = new MainAreaWidget({ content });
-    widget.id = 'react-mindmap-jupyterlab';
-    widget.title.label = 'ReactMindmap';
-    widget.title.closable = true;
-
+    let widget: MainAreaWidget<IFrame>;
     // Add an application command
     const command: string = 'react-mindmap:open';
     app.commands.addCommand(command, {
     label: 'Open React Mindmap',
     execute: () => {
+      if (!widget || widget.isDisposed) {
+        // Create a new widget if one does not exist
+        // or if the previous one was disposed after closing the panel
+        const content = new IFrame({
+          sandbox: ['allow-downloads' , 'allow-forms' , 'allow-modals' , 'allow-orientation-lock' , 'allow-pointer-lock' , 'allow-popups' , 'popups-to-escape-sandbox' , 'allow-presentation' , 'allow-same-origin' , 'allow-scripts' , 'allow-storage-access-by-user-activation' , 'allow-top-navigation' , 'allow-top-navigation-by-user-activation'],
+          referrerPolicy: 'unsafe-url'
+        });
+        content.url = "http://localhost:8080/react-mindmap/index.html";
+        widget = new MainAreaWidget({ content });
+        widget.id = 'react-mindmap-jupyterlab';
+        widget.title.label = 'ReactMindmap';
+        widget.title.closable = true;
+      }
+      if (!tracker.has(widget)) {
+        // Track the state of the widget for later restoration
+        tracker.add(widget);
+      }
       if (!widget.isAttached) {
         // Attach the widget to the main work area if it's not there
         app.shell.add(widget, 'main');
@@ -46,12 +54,20 @@ const activateFunc = async (app: JupyterFrontEnd, palette: ICommandPalette) => {
     selector: '*'
   }) 
 
+  let tracker = new WidgetTracker<MainAreaWidget<IFrame>>({
+    namespace: 'everjupyter-react-mindmap'
+  });
+  restorer.restore(tracker, {
+    command,
+    name: () => 'everjupyter-react-mindmap'
+  });
+
 }
 
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'react-mindmap',
   autoStart: true,
-  requires: [ICommandPalette],
+  requires: [ICommandPalette, ILayoutRestorer],
   activate: activateFunc
 };
 
